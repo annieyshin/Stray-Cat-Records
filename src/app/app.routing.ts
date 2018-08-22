5c08cb27c6141c62fa7a0c72dba97b7b64a2f707
Annie Shin and Nick Brown <annie@>
2018-08-22 10:53:15 -0700

    create admin component with routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent }   from './about/about.component';
 import { MarketplaceComponent }   from './marketplace/marketplace.component';
 import { AlbumDetailComponent }   from './album-detail/album-detail.component';
+import { AdminComponent } from './admin/admin.component';
 
 const appRoutes: Routes = [
   {
@@ -21,7 +22,11 @@ const appRoutes: Routes = [
  {
   path: 'albums/:id',
   component: AlbumDetailComponent
+  },
+  {
+  path: 'admin',
+  component: AdminComponent
   }
-  // Notice the dynamic :id segment in the route's path property above. By including the : before id we make the last portion of the path a variable instead of a literal string. :id will be replaced with the id number of the Album whose details we're viewing.
+
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);

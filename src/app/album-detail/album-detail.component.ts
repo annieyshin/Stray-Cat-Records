a02e8e93306a7f86de81b7b7809b0617d134d907
Annie Shin and Nick Brown <annie@>
2018-08-22 10:30:43 -0700

    Add the service to the album detail component

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 
 // Params will help us collect parameters for use when routing. Navigating to our Album detail page requires that we send the id of the Album we're attempting to view so we import this functionality to allow us to attach extra information to our request.
 
@@ -10,12 +11,18 @@ import { Album } from '../album.model';
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
-  styleUrls: ['./album-detail.component.css']
+  styleUrls: ['./album-detail.component.css'],
+  providers: [AlbumService]
 })
 export class AlbumDetailComponent implements OnInit {
-  albumId: number = null;
+  albumId: number;
+  albumToDisplay: Album;
 
-    constructor(private route: ActivatedRoute, private location: Location) {}
+    constructor(
+      private route: ActivatedRoute,
+      private location: Location,
+      private albumService: AlbumService
+    ) {}
 
     // By placing an instance of ActivatedRoute and Location in constructor(), we're ensuring that any new AlbumDetailComponent instances are created with these objects available to them.
 
@@ -24,17 +31,11 @@ export class AlbumDetailComponent implements OnInit {
     ngOnInit() {
      this.route.params.forEach((urlParameters) => {
        this.albumId = parseInt(urlParameters['id']);
-     });
-   }
+      });
+      this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
+    }
+    // Next, we'll add logic to the ngOnInit() method to retrieve the album AlbumDetailComponent should display from the service, using its new getAlbumById() method:.
 
-   // Gathers the ActivatedRoute object we placed in constructor() by calling this.route.
-
-  // Calls .params on the route to retrieve any parameters. Remember, we added parameters to the route in our goToDetailPage() method in the last lesson with the code this.router.navigate(['albums', clickedAlbum.id]);. This will return an array.
-
-  // Loops through all the parameters using forEach(), temporarily assigning each item parameter the variable name urlParameters.
-
-  // Retrieves the number in a key-value pair in urlParameters with the key id. (This is called id because we named it id in the dynamic segment of our route's path).
-
-  // Assigns the id value we retrieve to the AlbumDetailComponent's albumId property.
+    // This method passes our albumId property into our new getAlbumById method in our service, which returns the album we're interested in so that we can store it in the Album Details Component property albumToDisplay.
 
 }
--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -11,4 +11,16 @@ export class AlbumService {
   return ALBUMS;
   }
 
+  getAlbumById(albumId: number){
+    for (var i = 0; i <= ALBUMS.length - 1; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
+  }
+
+  // Now we can gather the specific Album object the user has requested to view from the service in the component's existing ngOnInit() method. However, right now our service only contains a method to return all Albums. Let's define another method that can return a specific Album:
+
+  // Here, we've defined a getAlbumById() method that takes the id of the Album we're seeking as a parameter. It simply loops through all Albums in our ALBUMS constant, and returns the one with the id we're looking for.
+
 }

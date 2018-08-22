d693c2d5e2342ff8bad4513fdb39ac7eb63a1690
Annie Shin and Nick Brown <annie@>
2018-08-22 09:50:20 -0700

    Create album detail component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent }   from './about/about.component';
 import { MarketplaceComponent }   from './marketplace/marketplace.component';
+import { AlbumDetailComponent }   from './album-detail/album-detail.component';
 
 const appRoutes: Routes = [
   {
@@ -16,6 +17,11 @@ const appRoutes: Routes = [
   {
    path: 'nick',
    component: MarketplaceComponent
+ },
+ {
+  path: 'albums/:id',
+  component: AlbumDetailComponent
   }
+  // Notice the dynamic :id segment in the route's path property above. By including the : before id we make the last portion of the path a variable instead of a literal string. :id will be replaced with the id number of the Album whose details we're viewing.
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
